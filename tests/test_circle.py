c6d23a5276c28c21d3be1018c3e42de215fe3a66
artem <artem.dan.com@gmail.com>
2024-12-21 02:12:08 +0300

    fix1

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -5,13 +5,13 @@ from circle import area, perimeter
 
 class TestCircle(unittest.TestCase):
 
-
     def test_area(self):
         self.assertEqual(area(1), math.pi)
         with self.assertRaises(ValueError):
             area(0)
         with self.assertRaises(ValueError):
             area(-1)
+
     def test_perimeter(self):
         self.assertEqual(perimeter(1), 2 * math.pi)
         with self.assertRaises(ValueError):
--- tests/test_square.py
+++ tests/test_square.py
@@ -4,13 +4,13 @@ from square import area, perimeter
 
 class TestSquare(unittest.TestCase):
 
-
     def test_area(self):
         self.assertEqual(area(2), 4)
         with self.assertRaises(ValueError):
             area(0)
         with self.assertRaises(ValueError):
             area(-1)
+
     def test_perimeter(self):
         self.assertEqual(perimeter(2), 8)
         with self.assertRaises(ValueError):
